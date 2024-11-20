5cb62f763a18e5e994e6ecfc05db637d09770bb3
Inkeri <inkeri.ahlstrom@helsinki.fi>
2024-11-20 10:53:37 +0200

    CSRF vulnerability

--- note_app/main/views.py
+++ note_app/main/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import get_object_or_404, render, redirect
 
 from django.views.decorators.csrf import csrf_exempt
 
+from django.contrib.auth.models import User
+
 from .models import Note
 from .forms import CreateNote, CreateUserForm, LoginForm
 
@@ -12,57 +14,42 @@ from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
-## Vulnerability:
-## Page can be accessed without logging in
-## How to fix:
-## add decorator @login_required(login_url="login") to fix access control
 def home(request):
     return render(request, "home.html")
 
-## Vulnerability:
-## Page can be accessed without logging in
-## How to fix:
-## add decorator @login_required(login_url="login") to fix access control
+@login_required(login_url="login")
 def notes(request):
     items = Note.objects.all()
     return render(request, "notes.html", {"notes": items})
 
-
 ## Vulnerability:
-## csrf_exempt decorator allows form to be created without csrf-protection
+## With no one logged in, anyone can add a new note without CSRF protection for ex. using:
+## curl -X POST http://127.0.0.1:8000/new_note/ \
+##     -d 'title=Attack&content=This is a malicous note.'
+## Note will be added by fake admin.
 ## How to fix:
-## Remove decorator, also add csrf_token in new_note.html template
-
-## Vulnerability:
-## Page can be accessed without logging in
-## How to fix:
-## add decorator @login_required(login_url="login") to fix access control
-@csrf_exempt
+## Add @login_required(login_url="login") to ensure user is logged in.
+## Remove if, else statements from lines 40-43, and replace with single line:
+##      newnote.author = request.user
 def new_note(request):
     form = CreateNote()
     if request.method == "POST":
         form = CreateNote(request.POST)
         if form.is_valid():
             newnote = form.save(commit=False)
-            newnote.author = request.user
+            if request.user.is_authenticated:
+                newnote.author = request.user
+            else:
+                newnote.author = User.objects.get(id=1)
             newnote.save()
             return redirect("notes")
 
     return render(request, "new_note.html", {"form": form})
 
-## Vulnerability:
-## Function uses raw SQL query to find note by given id.
-## How to fix:
-## remove get_note and replace note variable with following code:
-## note = get_object_or_404(Note, pk=note_id)
 
-## Vulnerability:
-## Page can be accessed without logging in
-## How to fix:
-## add decorator @login_required(login_url="login") to fix access control
+@login_required(login_url="login")
 def detail(request, note_id):
-    get_note = Note.objects.raw(f"SELECT * FROM main_note WHERE id LIKE '%{note_id}%'")
-    note = get_note[0]
+    note = get_object_or_404(Note, pk=note_id)
     return render(request, 'detail.html', {"note": note})
 
 ## Vulnerability:
@@ -75,11 +62,7 @@ def detail(request, note_id):
         ##note.delete()
         ##return redirect("notes")
     ##return render(request, 'detail.html', {"note": note})
-
-## Vulnerability:
-## Page can be accessed without logging in
-## How to fix:
-## add decorator @login_required(login_url="login") to fix access control
+@login_required(login_url="login")
 def delete(request, note_id):
     note = get_object_or_404(Note, pk=note_id)
     note.delete()
--- note_app/note_app/settings.py
+++ note_app/note_app/settings.py
@@ -44,7 +44,13 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+
+    ## Vulnerability:
+    ## CSRF protection middleware is disabled, leading to app not checking for CSRF.
+    ## How to fix:
+    ## Uncomment following line:
+
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
